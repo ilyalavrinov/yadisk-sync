6258f82e2c2622da894e0ab70e846e79fb59326c
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2018-09-12 18:42:16 +0300

    Base working version

--- Main.go
+++ Main.go
@@ -3,25 +3,42 @@ package main
 import "log"
 import "flag"
 import "os"
+import "time"
 import "io/ioutil"
 import "path"
+import "fmt"
+import "syscall"
+import "golang.org/x/crypto/ssh/terminal"
 
 const argFrom = "from"
 var fromPath = flag.String(argFrom, "", "File or directory which could be uploaded")
 
 const argTo = "to"
-var toPath = flag.String(argTo, "/", "Remote directory where everything will be uploaded")
+var toPath = flag.String(argTo, "", "Remote directory where everything will be uploaded")
+
+const argHost = "host"
+var host = flag.String(argHost, "https://webdav.yandex.ru", "WedDAV server hostname")
+
+const argThreads = "threads"
+var threadsNum = flag.Int(argThreads, 10, "Number of threads used for uploading")
+
+const argUser = "user"
+var user = flag.String(argUser, "", "Username used for authentication")
 
 func main() {
     flag.Parse()
     log.Printf("Arg: %s Value: %s", argFrom, *fromPath)
     log.Printf("Arg: %s Value: %s", argTo, *toPath)
+    log.Printf("Arg: %s Value: %d", argThreads, *threadsNum)
+    log.Printf("Arg: %s Value: %s", argUser, *user)
+    log.Printf("Arg: %s Value: %s", argHost, *host)
 
     isCorrectArgs := true
     if fromPath == nil || *fromPath == "" {
         isCorrectArgs = false
     }
-    if *toPath == "" {
+    if *user == "" {
+        // TODO: request from stdin same as password
         isCorrectArgs = false
     }
 
@@ -33,6 +50,24 @@ func main() {
 
     uploads := createUploadList(*fromPath, *toPath)
     log.Printf("Uploads: %+v", uploads)
+
+    pw := requestFromStdin("password")
+    opts := UploadOptions { Host: *host,
+                            User: *user,
+                            Password: pw }
+
+    uploadTasks := make(chan UploadTask, *threadsNum * 100)
+    for i := 0; i < *threadsNum; i++ {
+        uploader := NewUploader(opts, uploadTasks)
+        uploader.Run()
+    }
+
+    for _, u := range uploads {
+        uploadTasks<- u
+    }
+
+    log.Printf("Finished sending tasks")
+    time.Sleep(1 * time.Minute)
 }
 
 func createUploadList(fpath, uploadDir string) []UploadTask {
@@ -59,4 +94,11 @@ func createUploadList(fpath, uploadDir string) []UploadTask {
         panic("Unhandled path mode")
     }
     return result
+}
+
+
+func requestFromStdin(what string) string {
+    fmt.Print("Enter Password: ")
+    bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+    return string(bytePassword)
 }
--- Upload.go
+++ Upload.go
@@ -1,9 +1,14 @@
 package main
 
 import "log"
+import "os"
+import "github.com/studio-b12/gowebdav"
 
 type UploadOptions struct {
-    Token string
+    Host string
+    User string
+    Password string
+    // Token string    // not supported by the library currently
 }
 
 type UploadTask struct {
@@ -12,24 +17,36 @@ type UploadTask struct {
 
 type Uploader struct {
     opts UploadOptions
+    client gowebdav.Client
     ch <-chan UploadTask
 }
 
 func NewUploader(opts UploadOptions, tasks <-chan UploadTask) *Uploader {
     u := &Uploader{opts: opts,
+                   client: *gowebdav.NewClient(opts.Host, opts.User, opts.Password),
                    ch: tasks}
+
+    if err := u.client.Connect(); err != nil {
+        log.Fatalf("Could not open connection with settings %+v due to error: %s", u.opts, err)
+    }
     return u
 }
 
-func uploadOne(token string, from, to string) error {
-    panic("Not implemented")
+func uploadOne(client *gowebdav.Client, from, to string) error {
+    f, err := os.Open(from)
+    if err != nil {
+        log.Printf("Error during opening %s: %s", from, err)
+        return err
+    }
+    return client.WriteStream(to, f, os.ModePerm)
 }
 
 func (u *Uploader) Run() {
     go func() {
         task, notClosed := <-u.ch
         for ; notClosed; task, notClosed = <-u.ch {
-            err := uploadOne(u.opts.Token, task.From, task.To)
+            log.Printf("Uploading %s to %s", task.From, task.To)
+            err := uploadOne(&u.client, task.From, task.To)
             if err != nil {
                 log.Printf("Could not upload '%s' to '%s' due to error: %s", task.From, task.To, err)
             }
